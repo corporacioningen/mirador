00581913beff902da726549ce36d76325afc6bdd
corporacioningen <140711973+corporacioningen@users.noreply.github.com>
2024-07-06 15:49:35 +0200

    Create logout.js

---
+++ logout.js
@@ -0,0 +1,4 @@
+document.getElementById('logout-button').addEventListener('click', function() {
+    // Redirigir al usuario a la página de inicio de sesión
+    window.location.href = 'index.html';
+});
